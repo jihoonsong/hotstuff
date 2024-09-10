c0083ab8b6186610c2ada8a12b39d87c9f95df46
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-09-11 02:31:35 +0900

    Add private methods for each CoordinatorMessage

--- p2p/src/coordinator.rs
+++ p2p/src/coordinator.rs
@@ -40,30 +40,8 @@ impl Coordinator {
                 CoordinatorMessage::DialablePeers { respond } => {
                     respond.send(self.dialable_peers()).unwrap();
                 }
-                CoordinatorMessage::NewPeer { peer, mut stream } => {
-                    // If we have reached the maximum number of peers, just close the new connection.
-                    if self.connected_peers.len() >= self.max_peers as usize {
-                        let _ = stream.shutdown().await;
-                        info!("Shutdown new connection with {peer}: max peers reached");
-                        continue;
-                    }
-
-                    // Split the communication channel into writer and reader.
-                    let framed = Framed::new(stream, LengthDelimitedCodec::new());
-                    let (writer, reader) = framed.split();
-
-                    // TODO: After we have cryptography crate, we should use node's PublicKey as its identity.
-                    // We can use PublicKey to determine if the node is already connected. A handshake should
-                    // happen in advance to exchange identities.
-                    self.connected_peers.insert(peer, writer);
-
-                    // Spawn peer listening to its messages. Received messages will be redirected to Consensus.
-                    let hotstuff = self.hotstuff.clone();
-                    tokio::spawn(async move {
-                        Peer::new(peer, reader, hotstuff).run().await;
-                    });
-
-                    info!("New connected peer: {peer}");
+                CoordinatorMessage::NewPeer { peer, stream } => {
+                    self.new_peer(peer, stream).await;
                 }
             }
         }
@@ -80,5 +58,31 @@ impl Coordinator {
             .take(self.max_peers as usize - self.connected_peers.len())
             .cloned()
             .collect()
+    }
+
+    async fn new_peer(&mut self, peer: SocketAddr, mut stream: TcpStream) {
+        // If we have reached the maximum number of peers, just close the new connection.
+        if self.connected_peers.len() >= self.max_peers as usize {
+            let _ = stream.shutdown().await;
+            info!("Shutdown new connection with {peer}: max peers reached");
+            return;
+        }
+
+        // Split the communication channel into writer and reader.
+        let framed = Framed::new(stream, LengthDelimitedCodec::new());
+        let (writer, reader) = framed.split();
+
+        // TODO: After we have cryptography crate, we should use node's PublicKey as its identity.
+        // We can use PublicKey to determine if the node is already connected. A handshake should
+        // happen in advance to exchange identities.
+        self.connected_peers.insert(peer, writer);
+
+        // Spawn peer listening to its messages. Received messages will be redirected to Consensus.
+        let hotstuff = self.hotstuff.clone();
+        tokio::spawn(async move {
+            Peer::new(peer, reader, hotstuff).run().await;
+        });
+
+        info!("New connected peer: {peer}");
     }
 }
