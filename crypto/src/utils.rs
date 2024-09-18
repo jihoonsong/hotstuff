8aea2466cb66cbfb9913904cd89d51b67a79c7ee
syjn99 <syjn99@gmail.com>
2024-09-18 19:38:12 +0900

    Use string as common interface for users

--- crypto/src/utils.rs
+++ crypto/src/utils.rs
@@ -1,13 +1,30 @@
-use blsttc::{Error, PublicKeySet, PublicKeyShare, Signature, SignatureShare};
+use blsttc::{Error, PublicKeySet, PublicKeyShare, Signature, SignatureShare, PK_SIZE, SIG_SIZE};
+use hex;
 use std::collections::HashMap;
 
-pub fn verify(pk_share: PublicKeyShare, msg: &[u8], signature: &SignatureShare) -> bool {
-    pk_share.verify(signature, msg)
+pub fn verify(pk_share: String, msg: &[u8], signature_share: String) -> bool {
+    let pk_share: [u8; PK_SIZE] = hex::decode(pk_share).unwrap().try_into().unwrap();
+    let pk_share = PublicKeyShare::from_bytes(pk_share).unwrap();
+
+    let signature_share: [u8; SIG_SIZE] = hex::decode(signature_share).unwrap().try_into().unwrap();
+    let signature_share = SignatureShare::from_bytes(signature_share).unwrap();
+    pk_share.verify(&signature_share, msg)
 }
 
 pub fn combine_signatures(
-    pk_set: PublicKeySet,
-    signature_shares: HashMap<usize, SignatureShare>,
+    pk_set: String,
+    signature_shares: HashMap<usize, String>,
 ) -> Result<Signature, Error> {
+    let pk_set = PublicKeySet::from_bytes(hex::decode(pk_set).unwrap()).unwrap();
+
+    let signature_shares: HashMap<usize, SignatureShare> =
+        signature_shares
+            .iter()
+            .fold(HashMap::new(), |mut acc, (id, signature)| {
+                let signature_share: [u8; SIG_SIZE] =
+                    hex::decode(signature).unwrap().try_into().unwrap();
+                acc.insert(*id, SignatureShare::from_bytes(signature_share).unwrap());
+                acc
+            });
     pk_set.combine_signatures(signature_shares)
 }
