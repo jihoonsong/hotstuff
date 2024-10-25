1f79dab13b551d13254a6ac9893ca902f3d73c8a
syjn99 <syjn99@gmail.com>
2024-10-25 10:25:50 +0900

    Add unit tests

---
+++ crypto/tests/keypair_tests.rs
@@ -0,0 +1,17 @@
+use hotstuff_crypto::generate_random_keypairs;
+
+#[test]
+fn test_encode_hex() {
+    let (_, keypairs) = generate_random_keypairs(1, 1);
+    let keypair = &keypairs[0];
+    assert_eq!(keypair.pk.encode_hex(), hex::encode(keypair.pk.to_bytes()));
+}
+
+#[test]
+fn test_sign_and_verify() {
+    let (_, keypairs) = generate_random_keypairs(1, 1);
+    let keypair = &keypairs[0];
+    let message = b"test message";
+    let signature = keypair.sign(message.to_vec());
+    assert!(keypair.pk.verify(signature.to_bytes(), message.to_vec()));
+}
