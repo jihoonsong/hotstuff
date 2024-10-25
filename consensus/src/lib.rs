0f0c086c300cfd3c7bea4b5c0dae558e8c98a395
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-10-25 17:25:05 +0900

    Add `Block`

--- consensus/src/lib.rs
+++ consensus/src/lib.rs
@@ -1,9 +1,11 @@
+mod block;
 mod config;
 mod hotstuff;
 mod leader_elector;
 mod message;
 mod timeout;
 
+pub use block::Block;
 pub use config::HotStuffConfig;
 pub use hotstuff::HotStuff;
 pub use leader_elector::{LeaderElector, RoundRobinLeaderElector};
