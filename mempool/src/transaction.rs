c703832ee8b14cbe98312b5374180629f318582f
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-10-25 17:28:52 +0900

    Add nonce and data getters to `Transaction`

--- mempool/src/transaction.rs
+++ mempool/src/transaction.rs
@@ -22,12 +22,24 @@ pub struct MempoolTransaction {
 pub trait Transaction {
     fn hash(&self) -> String;
 
+    fn nonce(&self) -> u128;
+
+    fn data(&self) -> String;
+
     fn kind(&self) -> TransactionKind;
 }
 
 impl Transaction for MempoolTransaction {
     fn hash(&self) -> String {
         format!("0x{}", self.nonce) // TODO: Return cryptographic hash
+    }
+
+    fn nonce(&self) -> u128 {
+        self.nonce
+    }
+
+    fn data(&self) -> String {
+        self.data.clone()
     }
 
     fn kind(&self) -> TransactionKind {
